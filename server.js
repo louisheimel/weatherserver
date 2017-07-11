c5a0fadb2d0cb47015c8345e7d170c0d435e32e2
louisheimel1@gmail.com <louisheimel1@gmail.com>
2017-07-10 23:45:53 -0500

    allow requests from heroku site

--- server.js
+++ server.js
@@ -6,13 +6,16 @@ app.use((req, res, next) => {
 //  res.setHeader('Access-Control-Allow-Origin', 'https://sheltered-mesa-65680.herokuapp.com/')
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
   res.setHeader('Access-Control-Allow-Methods', 'GET')
+  next()
 })
 app.get('/', (req, res) => {
   res.end('something happened...')
 })
 
 app.get('/:lat/:long', (req, res) => {
-  var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + req.params.lat + '&lon=' + req.params.long + '&APPID=' + process.env.APPID
+  console.log('request received')
+  var allowable_url = 'https://nameless-citadel-16272.herokuapp.com/'
+  var url = allowable_url + req.params.lat + '&lon=' + req.params.long + '&APPID=' + process.env.APPID
   request(url, (err, response, body) => {
     if (err) {
       console.log(err)
