bce748532417ea1efe5b49bb47c91991a864cfd3
louisheimel1@gmail.com <louisheimel1@gmail.com>
2017-07-10 23:08:22 -0500

    change response headers

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ var express = require('express')
 var app = express()
 var request = require('request')
 
+app.use((req, res, next) => {
+//  res.setHeader('Access-Control-Allow-Origin', 'https://sheltered-mesa-65680.herokuapp.com/')
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.setHeader('Access-Control-Allow-Methods', 'GET')
+})
 app.get('/', (req, res) => {
   res.end('something happened...')
 })
+
 app.get('/:lat/:long', (req, res) => {
-  console.log('APPID is: ' + process.env.APPID)
   var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + req.params.lat + '&lon=' + req.params.long + '&APPID=' + process.env.APPID
-  console.log('serving!')
-  console.log(process.env)
   request(url, (err, response, body) => {
     if (err) {
       console.log(err)
@@ -20,7 +23,7 @@ app.get('/:lat/:long', (req, res) => {
     if (response) {
       console.log(response)
     }
-    res.json(response)
+    res.send(JSON.parse(response.body))
   })
 })
 
