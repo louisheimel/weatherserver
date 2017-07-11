1f7dd1f27230217ac182b2755ac473f75138d897
louisheimel1@gmail.com <louisheimel1@gmail.com>
2017-07-10 23:55:46 -0500

    allow access to sheltered mesa

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var request = require('request')
 
 app.use((req, res, next) => {
 //  res.setHeader('Access-Control-Allow-Origin', 'https://sheltered-mesa-65680.herokuapp.com/')
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.setHeader('Access-Control-Allow-Origin', 'https://nameless-citadel-16272.herokuapp.com://nameless-citadel-16272.herokuapp.com/' )
   res.setHeader('Access-Control-Allow-Methods', 'GET')
   next()
 })
@@ -14,8 +14,8 @@ app.get('/', (req, res) => {
 
 app.get('/:lat/:long', (req, res) => {
   console.log('request received')
-  var allowable_url = 'https://nameless-citadel-16272.herokuapp.com/'
-  var url = allowable_url + req.params.lat + '&lon=' + req.params.long + '&APPID=' + process.env.APPID
+  var api = 'http://api.openweathermap.org/data/2.5/weather?'
+  var url = api + req.params.lat + '&lon=' + req.params.long + '&APPID=' + process.env.APPID
   request(url, (err, response, body) => {
     if (err) {
       console.log(err)
